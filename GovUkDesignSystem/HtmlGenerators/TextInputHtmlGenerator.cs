330fb7d790056ed062c26b58e145ff78e34e5446
James Griffiths <james.griffiths@softwire.com>
2022-01-31 12:59:28 +0000

    Copy GovUkDesignSystem from https://github.com/cabinetoffice/gender-pay-gap

---
+++ GovUkDesignSystem/HtmlGenerators/TextInputHtmlGenerator.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Reflection;
+using GovUkDesignSystem.GovUkDesignSystemComponents;
+using GovUkDesignSystem.Helpers;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace GovUkDesignSystem.HtmlGenerators
+{
+    internal static class TextInputHtmlGenerator
+    {
+
+        internal static IHtmlContent GenerateHtml<TModel, TProperty>(
+            IHtmlHelper<TModel> htmlHelper,
+            Expression<Func<TModel, TProperty>> propertyLambdaExpression,
+            LabelViewModel labelOptions = null,
+            HintViewModel hintOptions = null,
+            FormGroupViewModel formGroupOptions = null,
+            string classes = null,
+            TextInputAppendixViewModel textInputAppendix = null,
+            string type = "text",
+            string autocomplete = null
+        )
+            where TModel : GovUkViewModel
+        {
+            PropertyInfo property = ExpressionHelpers.GetPropertyFromExpression(propertyLambdaExpression);
+
+            string propertyName = property.Name;
+
+            TModel model = htmlHelper.ViewData.Model;
+
+            string currentValue = ExtensionHelpers.GetCurrentValue(model, property, propertyLambdaExpression);
+
+            string id = $"GovUk_{propertyName}";
+            if (labelOptions != null)
+            {
+                labelOptions.For = id;
+            }
+
+            var textInputViewModel = new TextInputViewModel
+            {
+                Name = $"GovUk_Text_{propertyName}",
+                Id = id,
+                Value = currentValue,
+                Label = labelOptions,
+                Hint = hintOptions,
+                FormGroup = formGroupOptions,
+                Classes = classes,
+                TextInputAppendix = textInputAppendix,
+                Type = type,
+                Attributes = new Dictionary<string, string> {{"autocomplete", autocomplete}}
+            };
+
+            if (model.HasErrorFor(property))
+            {
+                textInputViewModel.ErrorMessage = new ErrorMessageViewModel {Text = model.GetErrorFor(property)};
+            }
+
+            return htmlHelper.Partial("/GovUkDesignSystemComponents/TextInput.cshtml", textInputViewModel);
+        }
+
+    }
+}
