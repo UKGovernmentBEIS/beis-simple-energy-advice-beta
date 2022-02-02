ef7e66edc54f19b1dea506545d4c1e2f6daca371
James Griffiths <james.griffiths@softwire.com>
2022-02-02 14:14:29 +0000

    A few pages to get started with

---
+++ SeaPublicWebsite/Controllers/EnergyEfficiencyController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SeaPublicWebsite.Controllers
+{
+    [Route("energy-efficiency")]
+    public class EnergyEfficiencyController : Controller
+    {
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
