6ca2b216fd40ba69c1a12b1a8d97d5f11ea96b08
James Griffiths <james.griffiths@softwire.com>
2022-02-03 18:35:14 +0000

    Simple ask-for-postcode page (to test connection to postcodes.io)

--- SeaPublicWebsite/Controllers/EnergyEfficiencyController.cs
+++ SeaPublicWebsite/Controllers/EnergyEfficiencyController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GovUkDesignSystem;
+using GovUkDesignSystem.Parsers;
+using Microsoft.AspNetCore.Mvc;
+using SeaPublicWebsite.ExternalServices;
+using SeaPublicWebsite.Models.EnergyEfficiency;
 
 namespace SeaPublicWebsite.Controllers
 {
@@ -8,7 +12,38 @@ namespace SeaPublicWebsite.Controllers
         [HttpGet("")]
         public IActionResult Index()
         {
-            return View();
+            return View("Index");
+        }
+        
+        [HttpGet("postcode")]
+        public IActionResult AskForPostcode_Get()
+        {
+            var viewModel = new AskForPostcodeViewModel();
+            
+            return View("AskForPostcode", viewModel);
+        }
+        
+        [HttpPost("postcode")]
+        public IActionResult AskForPostcode_Post(AskForPostcodeViewModel viewModel)
+        {
+            viewModel.ParseAndValidateParameters(Request, m => m.Postcode);
+
+            if (viewModel.HasAnyErrors())
+            {
+                return View("AskForPostcode", viewModel);
+            }
+
+            if (!PostcodesIoApi.IsValidPostcode(viewModel.Postcode))
+            {
+                viewModel.AddErrorFor(m => m.Postcode, "Enter a valid UK post code");
+            }
+
+            if (viewModel.HasAnyErrors())
+            {
+                return View("AskForPostcode", viewModel);
+            }
+
+            return Json(new {Success = "Yes!"});
         }
     }
 }
---
+++ SeaPublicWebsite/Models/EnergyEfficiency/AskForPostcodeViewModel.cs
@@ -0,0 +1,11 @@
+﻿using GovUkDesignSystem;
+using GovUkDesignSystem.Attributes.ValidationAttributes;
+
+namespace SeaPublicWebsite.Models.EnergyEfficiency
+{
+    public class AskForPostcodeViewModel : GovUkViewModel
+    {
+        [GovUkValidateRequired(ErrorMessageIfMissing = "Enter a UK post code")]
+        public string Postcode { get; set; }
+    }
+}
