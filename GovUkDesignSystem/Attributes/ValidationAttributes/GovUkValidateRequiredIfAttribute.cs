3faf9714386608dbd1f97cab67b71e1bdf9d9377
CodCot <codrin.cotarlan@softwire.com>
2022-04-29 09:47:28 +0100

    feat: Add ValidateRequiredIf attribute

---
+++ GovUkDesignSystem/Attributes/ValidationAttributes/GovUkValidateRequiredIfAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace GovUkDesignSystem.Attributes.ValidationAttributes
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class GovUkValidateRequiredIfAttribute: ValidationAttribute
+    {
+        private readonly string requiredError;
+        private readonly bool isRequired;
+
+        public GovUkValidateRequiredIfAttribute(string requiredError, bool isRequired)
+        {
+            this.requiredError = requiredError;
+            this.isRequired = isRequired;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (isRequired && value is null)
+            {
+                return new ValidationResult(requiredError);
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
