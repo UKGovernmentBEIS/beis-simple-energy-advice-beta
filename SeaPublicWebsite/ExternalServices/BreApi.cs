2ca1a6ee2ab20ff724ff2e2d39886ee2e63dbad2
Nat Dean-Lewis <Nat.Dean-Lewis@softwire.com>
2022-05-04 14:48:36 +0100

    refactor: bre api class

--- SeaPublicWebsite/ExternalServices/BreApi.cs
+++ SeaPublicWebsite/ExternalServices/BreApi.cs
@@ -1,7 +1,92 @@
-﻿namespace SeaPublicWebsite.ExternalServices
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Cryptography;
+using System.Text;
+using Newtonsoft.Json.Linq;
+using SeaPublicWebsite.DataModels;
+using SeaPublicWebsite.Services;
+
+namespace SeaPublicWebsite.ExternalServices
 {
-    public class BreApi
+    public static class BreApi
     {
-    
+        public static List<Recommendation> GetRecommendationsForUserRequest(string request)
+        {
+            try
+            {
+                using (HttpClient httpClient = new HttpClient())
+                {
+                    httpClient.BaseAddress = new Uri("https://uat.brewebserv.com");
+
+                    string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+                    Random random = new();
+                    string nonce = new string(Enumerable.Repeat(chars, 64)
+                        .Select(s => s[random.Next(s.Length)]).ToArray());
+                    string created = DateTime.Now.ToUniversalTime().ToString
+                        (DateTimeFormatInfo.InvariantInfo.SortableDateTimePattern) + "Z";
+                    string username = "softwire0.0.1";
+                    string password = "9b052a18df10baade8c7448f3";
+                    string token;
+                    using (SHA256 hash = SHA256.Create())
+                    {
+                        token = string.Concat(hash
+                            .ComputeHash(Encoding.UTF8.GetBytes(password + nonce + username + created))
+                            .Select(item => item.ToString("x2")));
+                    }
+
+                    string wsseHeader =
+                        $"WSSE UsernameToken Token=\"{token}\", Nonce=\"{nonce}\", Username=\"{username}\", Created=\"{created}\"";
+                    httpClient.DefaultRequestHeaders.Add("Authorization", wsseHeader);
+
+                    string path = "/bemapi/energy_use";
+                    StringContent stringContent = new(request);
+
+                    HttpResponseMessage response = httpClient.PostAsync(path, stringContent).Result;
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string bodyString = response.Content.ReadAsStringAsync().Result;
+                        JObject measures = JObject.FromObject(JObject.Parse(bodyString)["measures"]);
+
+                        List<Recommendation> recommendations = new List<Recommendation>();
+                        foreach (JProperty prop in measures.Properties())
+                        {
+                            Dictionary<string, Recommendation> recommendationDictionary =
+                                RecommendationService.RecommendationDictionary;
+                            JToken value = prop.Value;
+                            int minInstallCost = (int) value["min_installation_cost"];
+                            int maxInstallCost = (int) value["max_installation_cost"];
+                            int saving = (int) value["cost_saving"];
+                            int lifetime = (int) value["lifetime"];
+
+                            Recommendation recommendation = new()
+                            {
+                                Key = recommendationDictionary[prop.Name].Key,
+                                Title = recommendationDictionary[prop.Name].Title,
+                                MinInstallCost = minInstallCost,
+                                MaxInstallCost = maxInstallCost,
+                                Saving = saving,
+                                LifetimeSaving = lifetime * saving,
+                                Lifetime = lifetime,
+                                Summary = recommendationDictionary[prop.Name].Summary
+                            };
+                            recommendations.Add(recommendation);
+                        }
+
+                        Console.WriteLine($"{recommendations.Count} recommendations found");
+                        return recommendations;
+                    }
+
+                    return null;
+                }
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
     }
 }
--- SeaPublicWebsite/Services/RecommendationService.cs
+++ SeaPublicWebsite/Services/RecommendationService.cs
@@ -1,13 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Net.Http;
-using System.Security.Cryptography;
-using System.Text;
+﻿using System.Collections.Generic;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 using SeaPublicWebsite.DataModels;
+using SeaPublicWebsite.ExternalServices;
 using SeaPublicWebsite.ExternalServices.Models;
 using SeaPublicWebsite.Models.EnergyEfficiency;
 
@@ -15,56 +9,16 @@ namespace SeaPublicWebsite.Services
 {
     public static class RecommendationService
     {
-        public static List<Recommendation> GetRecommendationsForUser(UserDataModel userData)
-        {
-            var exampleCase3Request = new BreRequest
+        public static readonly Dictionary<string, Recommendation> RecommendationDictionary =
+            new()
             {
-                epc = new RequestEpc
-                {
-                    postcode = "A12 3BC",
-                    isConnectedtoMainsGas = true,
-                    propertyType = "house",
-                    builtForm = "mid-terrace",
-                    floorLevel = null,
-                    heatLossCorridor = "no corridor",
-                    numberHabitableRooms = 7,
-                    totalFloorArea = "151.26",
-                    wallsDescription = "Solid brick, as built, no insulation (assumed)",
-                    roofDescription = "Pitched, no insulation (assumed)",
-                    multiGlazeProportion = "30",
-                    glazedArea = "Normal",
-                    mainheatDescription = "boiler and radiators, mains gas",
-                    mainheatcontDescription = "Programmer, no room thermostat",
-                    numberOpenFireplaces = "1",
-                    lowEnergyLighting = "80",
-                    solarWaterHeatingFlag = "N",
-                    photoSupply = "0",
-                    windTurbineCount = "0"
-                },
-                construction_date = "C",
-                heating_fuel = "26",
-                electricity_tariff = 1,
-                num_storeys = 2,
-                hot_water_cylinder = true,
-                condensing_boiler = true,
-                heating_pattern_type = 3,
-                normal_days_off_hours = new[] {7, 9},
-                living_room_temperature = 22,
-                showers_per_week = 0,
-                baths_per_week = 7,
-                measures = true,
-                measures_package = new[] {"A", "W1", "G", "U"}
-            };
-
-            var recommendationDictionary = new Dictionary<string, Recommendation>
-            {
+                //Not all possible measures from the bre api are here yet
                 {
                     "A", new Recommendation
                     {
                         Key = RecommendationKey.AddLoftInsulation,
                         Title = "Add some loft insulation",
-                        Summary =
-                            "Increase the level of insulation in your loft to the recommended level of 300mm"
+                        Summary = "Increase the level of insulation in your loft to the recommended level of 300mm"
                     }
                 },
                 {
@@ -100,84 +54,52 @@ namespace SeaPublicWebsite.Services
                     }
                 }
             };
-            try
+
+        public static List<Recommendation> GetRecommendationsForUser(UserDataModel userData)
+        {
+            // Just using example data for now, extracting epc and other request params from userData to come
+            BreRequest exampleCase3Request = new BreRequest
             {
-                using (var httpClient = new HttpClient())
+                epc = new RequestEpc
                 {
-                    var request = JsonConvert.SerializeObject(exampleCase3Request);
-                    const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-                    Random random = new();
-                    var nonce = new string(Enumerable.Repeat(chars, 64)
-                        .Select(s => s[random.Next(s.Length)]).ToArray());
-                    var created = DateTime.Now.ToUniversalTime().ToString
-                        (DateTimeFormatInfo.InvariantInfo.SortableDateTimePattern) + "Z";
-                    var username = "softwire0.0.1";
-                    var password = "9b052a18df10baade8c7448f3";
+                    postcode = "A12 3BC",
+                    isConnectedtoMainsGas = true,
+                    propertyType = "house",
+                    builtForm = "mid-terrace",
+                    floorLevel = null,
+                    heatLossCorridor = "no corridor",
+                    numberHabitableRooms = 7,
+                    totalFloorArea = "151.26",
+                    wallsDescription = "Solid brick, as built, no insulation (assumed)",
+                    roofDescription = "Pitched, no insulation (assumed)",
+                    multiGlazeProportion = "30",
+                    glazedArea = "Normal",
+                    mainheatDescription = "boiler and radiators, mains gas",
+                    mainheatcontDescription = "Programmer, no room thermostat",
+                    numberOpenFireplaces = "1",
+                    lowEnergyLighting = "80",
+                    solarWaterHeatingFlag = "N",
+                    photoSupply = "0",
+                    windTurbineCount = "0"
+                },
+                construction_date = "C",
+                heating_fuel = "26",
+                electricity_tariff = 1,
+                num_storeys = 2,
+                hot_water_cylinder = true,
+                condensing_boiler = true,
+                heating_pattern_type = 3,
+                normal_days_off_hours = new[] { 7, 9 },
+                living_room_temperature = 22,
+                showers_per_week = 0,
+                baths_per_week = 7,
+                measures = true,
+                measures_package = new[] { "A", "W1", "G", "U" }
+            };
 
-                    string sha256_hash(string value)
-                    {
-                        using (var hash = SHA256.Create())
-                        {
-                            return string.Concat(hash
-                                .ComputeHash(Encoding.UTF8.GetBytes(value))
-                                .Select(item => item.ToString("x2")));
-                        }
-                    }
+            string request = JsonConvert.SerializeObject(exampleCase3Request);
 
-                    var token = sha256_hash(password + nonce + username + created);
-                    var wsse_header = string.Join("", "WSSE UsernameToken ", "Token=\"", token, "\", ", "Nonce=\"",
-                        nonce,
-                        "\", ",
-                        "Username=\"", username, "\", ", "Created=\"", created, "\"");
-                    httpClient.DefaultRequestHeaders.Add("Authorization", wsse_header);
-                    httpClient.BaseAddress = new Uri("https://uat.brewebserv.com");
-
-                    var path = "/bemapi/energy_use";
-                    StringContent stringContent = new(request);
-
-                    var response = httpClient.PostAsync(path, stringContent).Result;
-
-                    if (response.IsSuccessStatusCode)
-                    {
-                        var bodyString = response.Content.ReadAsStringAsync().Result;
-                        var measures = JObject.FromObject(JObject.Parse(bodyString)["measures"]);
-
-                        var recommendations = new List<Recommendation>();
-                        foreach (var prop in measures.Properties())
-                        {
-                            var value = prop.Value;
-                            var minInstallCost = (int) value["min_installation_cost"];
-                            var maxInstallCost = (int) value["max_installation_cost"];
-                            var saving = (int) value["cost_saving"];
-                            var lifetime = (int) value["lifetime"];
-
-                            Recommendation recommendation = new()
-                            {
-                                Key = recommendationDictionary[prop.Name].Key,
-                                Title = recommendationDictionary[prop.Name].Title,
-                                MinInstallCost = minInstallCost,
-                                MaxInstallCost = maxInstallCost,
-                                Saving = saving,
-                                LifetimeSaving = lifetime * saving,
-                                Lifetime = lifetime,
-                                Summary = recommendationDictionary[prop.Name].Summary
-                            };
-                            recommendations.Add(recommendation);
-                        }
-
-                        foreach (var recommendation in recommendations)
-                            Console.WriteLine($"{recommendation.Title}: {recommendation.Summary}");
-
-                        return recommendations;
-                    }
-
-                    return null;
-                }
-            }
-            catch (Exception)
-            {
-                return null;
-            }
+            return BreApi.GetRecommendationsForUserRequest(request);
         }
     }
 }
