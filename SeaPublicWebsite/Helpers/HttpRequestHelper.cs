249ba706e79682c96dfcba5a33257c09fa9450ae
CodCot <codrin.cotarlan@softwire.com>
2022-05-05 14:48:47 +0100

    Refactor: Create helper class to send HTTP requests

---
+++ SeaPublicWebsite/Helpers/HttpRequestHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+
+namespace SeaPublicWebsite.Helpers
+{
+    public static class HttpRequestHelper
+    {
+        public static T SendGetRequest<T>(string baseAddress, string path, AuthenticationHeaderValue auth)
+        {
+            var httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri(baseAddress);
+            httpClient.DefaultRequestHeaders.Authorization = auth;
+            httpClient.DefaultRequestHeaders.Add("Accept", "application/json");
+            var response = httpClient.GetAsync(path).Result;
+
+            if (!response.IsSuccessStatusCode) return default;
+            
+            var bodyString = response.Content.ReadAsStringAsync().Result;
+            return JsonConvert.DeserializeObject<T>(bodyString);
+        }
+
+        public static string ConvertToBase64(string username, string password)
+        {
+            return Convert.ToBase64String(System.Text.Encoding.ASCII.GetBytes($"{username}:{password}"));
+        }
+    }
+}
