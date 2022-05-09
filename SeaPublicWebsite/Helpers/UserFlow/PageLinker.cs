9d2e754fffc13aa970965f6e378d61c81310c6ce
CodCot <codrin.cotarlan@softwire.com>
2022-05-09 11:10:50 +0100

    feat: Add empty back link methods for all question pages

--- SeaPublicWebsite/Helpers/UserFlow/PageLinker.cs
+++ SeaPublicWebsite/Helpers/UserFlow/PageLinker.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Routing;
 
 namespace SeaPublicWebsite.Helpers.UserFlow
@@ -30,5 +31,154 @@ namespace SeaPublicWebsite.Helpers.UserFlow
                 ? linkGenerator.GetPathByAction("AnswerSummary", "EnergyEfficiency", new { reference })
                 : linkGenerator.GetPathByAction("Country_Get", "EnergyEfficiency", new { reference });
         }
+
+        public string CountryBackLink()
+        {
+            throw new Exception();
+        }
+
+        public string ServiceUnsuitableBackLink()
+        {
+            throw new Exception();
+            
+        }
+
+        public string AskForPostcodeBackLink()
+        {
+            throw new Exception();
+        }
+
+        public string ConfirmAddressBackLink()
+        {
+            throw new Exception();
+        }
+
+        public string PropertyTypeBackLink()
+        {
+            throw new Exception();
+        }
+        
+        public string HouseTypeBackLink()
+        {
+            throw new Exception();
+        }
+        
+        public string BungalowTypeBackLink()
+        {
+            throw new Exception();
+        }
+        
+        public string FlatTypeBackLink()
+        {
+            throw new Exception();
+        }
+        
+        public string HomeAgeBackLink()
+        {
+            throw new Exception();
+        }
+        
+        public string WallConstructionBackLink()
+        {
+            throw new Exception();
+        }
+        
+        public string CavityWallsInsulatedBackLink()
+        {
+            throw new Exception();
+        }
+        
+        public string SolidWallsInsulatedBackLink()
+        {
+            throw new Exception();
+        }
+        
+        public string FloorConstructionBackLink()
+        {
+            throw new Exception();
+        }
+        
+        public string FloorInsulatedBackLink()
+        {
+            throw new Exception();
+        }
+        
+        public string RoofConstructionBackLink()
+        {
+            throw new Exception();
+        }
+        
+        public string AccessibleLoftSpaceBackLink()
+        {
+            throw new Exception();
+        }
+        
+        public string RoofInsulatedBackLink()
+        {
+            throw new Exception();
+        }
+        
+        public string OutdoorSpaceBackLink()
+        {
+            throw new Exception();
+        }
+        
+        public string GlazingTypeBackLink()
+        {
+            throw new Exception();
+        }
+        
+        public string HeatingTypeBackLink()
+        {
+            throw new Exception();
+        }
+        
+        public string OtherHeatingTypeBackLink()
+        {
+            throw new Exception();
+        }
+        
+        public string HotWaterCylinderBackLink()
+        {
+            throw new Exception();
+        }
+        
+        public string NumberOfOccupantsBackLink()
+        {
+            throw new Exception();
+        }
+        
+        public string HeatingPatternBackLink()
+        {
+            throw new Exception();
+        }
+        
+        public string TemperatureBackLink()
+        {
+            throw new Exception();
+        }
+        
+        // Ghost page
+        public string EmailAddressBackLink()
+        {
+            throw new Exception();
+        }
+        
+        public string AnswerSummaryBackLink()
+        {
+            throw new Exception();
+        }
+        
+        public string YourRecommendationsBackLink()
+        {
+            throw new Exception();
+        }
+        
+        public string BackLink()
+        {
+            throw new Exception();
+        }
+        
+        // ^ Copy paste template ^
     }
 }
