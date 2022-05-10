4e48d96459f1945481c8b2da9bdfd4151450ca7f
Nat Dean-Lewis <Nat.Dean-Lewis@softwire.com>
2022-05-10 15:00:11 +0100

    refactor: remove unused fields

--- SeaPublicWebsite/ExternalServices/Models/BreRequest.cs
+++ SeaPublicWebsite/ExternalServices/Models/BreRequest.cs
@@ -16,38 +16,24 @@
 
         public string? flat_level { get; set; }
 
-        // public RequestEpc? epc { get; set; }
-
         public string? postcode { get; set; }
 
-        // public int? electricity_tariff { get; set; }
-
         public bool? hot_water_cylinder { get; set; }
-
-        // public bool? condensing_boiler { get; set; }
 
         public decimal? living_room_temperature { get; set; }
 
         public int? heating_pattern_type { get; set; }
-        
-        // public int[]? normal_days_off_hours { get; set; }
 
         public int? occupants { get; set; }
 
-        // public int? shower_type { get; set; }
-
-        // public int? showers_per_week { get; set; }
-
-        // public int? baths_per_week { get; set; }
-                
         public bool? measures { get; set; }
-        
+
         public string[]? measures_package { get; set; }
-        
+
         public int? roof_type { get; set; }
-        
+
         public int? wall_type { get; set; }
-        
+
         public int? glazing_type { get; set; }
     }
 }
