f321191d909eeb9b581009a772c21a1a36b802c5
CodCot <codrin.cotarlan@softwire.com>
2022-05-13 14:47:45 +0100

    feat: Prevent banner to show on cookie page

--- GovUkDesignSystem/GovUkDesignSystemComponents/CookieBannerViewModel.cs
+++ GovUkDesignSystem/GovUkDesignSystemComponents/CookieBannerViewModel.cs
@@ -5,8 +5,16 @@ namespace GovUkDesignSystem.GovUkDesignSystemComponents;
 public class CookieBannerViewModel
 {
     public string NameOfService { get; set; }
+    public BannerState BannerState { get; set; } 
     public CookieType CookieType { get; set; }
     public string ButtonClickAction { get; set; }
     public string ViewCookiesLink { get; set; }
     public string ReturnUrl { get; set; }
+}
+
+public enum BannerState
+{
+    ShowBanner,
+    ShowDecision,
+    Hide
 }
