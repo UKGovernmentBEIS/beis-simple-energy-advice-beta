a6d1ef323e4e95cf696762dbef207a7591c96172
CodCot <codrin.cotarlan@softwire.com>
2022-05-13 09:41:21 +0100

    feat: Add basic cookies controller

---
+++ SeaPublicWebsite/Controllers/CookiesController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SeaPublicWebsite.Controllers;
+
+public class CookiesController: Controller
+{
+
+    [HttpGet("/cookies")]
+    public IActionResult CookieSettingsGet()
+    {
+        throw new Exception();
+    }
+
+    [HttpPost("/cookies")]
+    [ValidateAntiForgeryToken]
+    public IActionResult CookieSettingsPost()
+    {
+        throw new Exception();
+    }
+
+    [HttpPost("/cookie-consent")]
+    [ValidateAntiForgeryToken]
+    public IActionResult CookieConsent()
+    {
+        throw new Exception();
+    }
+
+    [HttpGet("/cookie-details")]
+    public IActionResult CookieDetails()
+    {
+        throw new Exception();
+    }
+
+}
