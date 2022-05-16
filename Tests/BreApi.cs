2fb152d3b67f6100811183e9acc0459b771c5946
Nat Dean-Lewis <Nat.Dean-Lewis@softwire.com>
2022-05-16 16:50:03 +0100

    feat: add basic test that BreRequest constructor requests measures in response

---
+++ Tests/BreApi.cs
@@ -0,0 +1,34 @@
+using NUnit.Framework;
+using SeaPublicWebsite.ExternalServices.Models;
+using FluentAssertions;
+
+namespace Tests;
+
+public class BreApi
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void RequestsMeasures()
+    {
+        BreRequest request = new(
+            brePostcode: "",
+            brePropertyType: BrePropertyType.House,
+            breBuiltForm: BreBuiltForm.Detached,
+            breFlatLevel: null,
+            breConstructionDate: "A",
+            breWallType: BreWallType.CavityWallsWithoutInsulation,
+            breRoofType: BreRoofType.PitchedRoofWithoutInsulation,
+            breGlazingType: BreGlazingType.SingleGlazed,
+            breHeatingFuel: BreHeatingFuel.MainsGas,
+            breHotWaterCylinder: null,
+            breOccupants: null,
+            breHeatingPatternType: BreHeatingPatternType.MorningAndEvening,
+            breTemperature: null
+        );       
+        request.measures.Should().Be(true);
+    }
+}
