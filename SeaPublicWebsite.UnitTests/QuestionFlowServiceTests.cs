c9cc2394151d183b641e0683b5c239965c767e39
CodCot <codrin.cotarlan@softwire.com>
2022-05-18 17:07:53 +0100

    test: Add skeleton test file for links

---
+++ SeaPublicWebsite.UnitTests/QuestionFlowServiceTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using SeaPublicWebsite.Services;
+
+namespace Tests;
+
+public class QuestionFlowServiceTests
+{
+    private IQuestionFlowService QuestionFlowService;
+
+    public QuestionFlowServiceTests()
+    { }
+
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void RequestsMeasures()
+    {
+        //Arrange
+        //Act
+        //Assert
+    }
+}
