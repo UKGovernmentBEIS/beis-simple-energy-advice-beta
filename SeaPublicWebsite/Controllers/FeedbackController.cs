bf2972349cd65fe3eb2f16cf2baa9aee18e75a58
CodCot <codrin.cotarlan@softwire.com>
2022-05-20 12:30:31 +0100

    feat: Add initial feedback form page

---
+++ SeaPublicWebsite/Controllers/FeedbackController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SeaPublicWebsite.Models.Feedback;
+
+namespace SeaPublicWebsite.Controllers;
+
+[Route("feedback")]
+public class FeedbackController : Controller
+{
+    [HttpGet("send-feedback")]
+    public IActionResult SendFeedback_Get()
+    {
+        var viewModel = new FeedbackFormViewModel();
+        return View("FeedbackForm", viewModel);
+    }
+
+    [HttpPost("send-feedback")]
+    public IActionResult SendFeedback_Post(FeedbackFormViewModel viewModel)
+    {
+        return View("FeedbackForm", viewModel);
+    }
+}
