469cce4eb39b4f3a9abadaab080f1d309d919b70
Nat Dean-Lewis <Nat.Dean-Lewis@softwire.com>
2022-05-26 10:44:58 +0100

    feat: change Url.Action for form, slight reformatting

--- SeaPublicWebsite/Models/EnergyEfficiency/QuestionOptions/HasEmailAddress.cs
+++ SeaPublicWebsite/Models/EnergyEfficiency/QuestionOptions/HasEmailAddress.cs
@@ -5,6 +5,7 @@ namespace SeaPublicWebsite.Models.EnergyEfficiency.QuestionOptions
     public enum HasEmailAddress
     {
         [GovUkRadioCheckboxLabelText(Text = "Yes, email a link and the reference number to me")]
-        Yes
+        Yes,
+        No
     }
 }
