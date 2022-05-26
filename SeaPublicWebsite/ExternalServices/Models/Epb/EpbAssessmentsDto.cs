845a6cae384842eb0265ddd989748f8f0ec053b6
CodCot <codrin.cotarlan@softwire.com>
2022-05-26 14:45:59 +0100

    feat: Add Dto models for EpbEpc responses

---
+++ SeaPublicWebsite/ExternalServices/Models/Epb/EpbAssessmentsDto.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace SeaPublicWebsite.ExternalServices.Models.Epb;
+
+public class EpbAssessmentsDto
+{
+    [JsonProperty(PropertyName = "data")]
+    public EpbAssessmentsDataDto Data { get; set; }
+}
+
+public class EpbAssessmentsDataDto
+{
+    [JsonProperty(PropertyName = "assessments")]
+    public List<EpbAssessmentId> Assessments { get; set; }
+}
+
+public class EpbAssessmentId
+{
+    [JsonProperty(PropertyName = "epcRrn")]
+    public string EpcId { get; set; }
+    
+    [JsonProperty(PropertyName = "address")]
+    public EpbAddressDto Address { get; set; }
+}
