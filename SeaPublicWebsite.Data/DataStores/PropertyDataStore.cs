b43390bbbc75f2a1fa8d3fa5d182beea21863876
Nat Dean-Lewis <Nat.Dean-Lewis@softwire.com>
2022-05-27 11:43:18 +0100

    feat: add dummy PropertyDataStore function - LoadPropertyData, IsReferenceValid, SavePropertyData need to be made to reference the postgres db

---
+++ SeaPublicWebsite.Data/DataStores/PropertyDataStore.cs
@@ -0,0 +1,38 @@
+﻿using SeaPublicWebsite.Helpers;
+
+namespace SeaPublicWebsite.Data.DataStores;
+
+public class PropertyDataStore
+{
+    public PropertyData LoadPropertyData(string reference)
+    {
+        return new PropertyData();
+    }
+
+    public bool IsReferenceValid(string reference)
+    {
+        return true;
+    }
+
+    public void SavePropertyData(PropertyData propertyData)
+    {
+
+    }
+
+    public string GenerateNewReferenceAndSaveEmptyPropertyData()
+    {
+        string reference;
+        do
+        {
+            reference = RandomHelper.Generate8DigitReference();
+        } while (IsReferenceValid(reference));
+
+        PropertyData propertyData = new()
+        {
+            Reference = reference
+        };
+        SavePropertyData(propertyData);
+
+        return reference;
+    }
+}
---
+++ SeaPublicWebsite.Data/Helpers/RandomHelper.cs
@@ -0,0 +1,18 @@
+﻿namespace SeaPublicWebsite.Data.Helpers
+{
+    public class RandomHelper
+    {
+        public static string Generate8DigitReference()
+        {
+            string characters = "ABCDEFGHJKLMNPQRSTUVWXYZ23456789";
+            string reference = "";
+
+            for (int i = 0; i < 8; i++)
+            {
+                reference += characters[new Random().Next(0, characters.Length)];
+            }
+            
+            return reference;
+        }
+    }
+}
