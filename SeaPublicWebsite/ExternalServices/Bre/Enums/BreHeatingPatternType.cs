1c327e4f256c91f3c495faacc222f5db201710d0
Nat Dean-Lewis <Nat.Dean-Lewis@softwire.com>
2022-06-06 16:18:07 +0100

    feat: remove unused BreHeatingPatternType.cs values

--- SeaPublicWebsite/ExternalServices/Bre/Enums/BreHeatingPatternType.cs
+++ SeaPublicWebsite/ExternalServices/Bre/Enums/BreHeatingPatternType.cs
@@ -6,8 +6,6 @@
         AllDayAndAllNight = 1,
         AllDayButOffAtNight = 2,
         MorningAndEvening = 3,
-        JustOnceADay = 4,
-        IDontKnow = 5,
         NoneOfTheAbove = 6,
     }
 }
