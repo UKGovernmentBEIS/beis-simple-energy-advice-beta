3bde881d12d79debf085aebc3825b76859e3baa1
Nat Dean-Lewis <Nat.Dean-Lewis@softwire.com>
2022-06-09 17:14:47 +0100

    feat: include designer with epc migration

---
+++ SeaPublicWebsite.Data/Migrations/20220609161351_Epc.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SeaPublicWebsite.Data.Migrations
+{
+    public partial class Epc : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
