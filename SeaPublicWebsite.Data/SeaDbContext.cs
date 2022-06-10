447fdca26a3d4aacf67eea04a2f24c19062fc480
Dan Corder <dan.corder@softwire.com>
2022-06-10 16:39:00 +0100

    feat: Add unique index to reference code

--- SeaPublicWebsite.Data/SeaDbContext.cs
+++ SeaPublicWebsite.Data/SeaDbContext.cs
@@ -5,11 +5,18 @@ namespace SeaPublicWebsite.Data;
 
 public class SeaDbContext : DbContext
 {
-    public SeaDbContext(DbContextOptions<SeaDbContext> options) : base(options)
-    {
-        
-    }
     public DbSet<PropertyData> PropertyData { get; set; }
     public DbSet<Epc> Epc { get; set; }
     public DbSet<PropertyRecommendation> PropertyRecommendations { get; set; }
+    
+    public SeaDbContext(DbContextOptions<SeaDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<PropertyData>()
+            .HasIndex(p => p.Reference)
+            .IsUnique();
+    }
 }
