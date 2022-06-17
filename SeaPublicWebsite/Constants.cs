0aee192f3d4507a91c2c454d7bddae42d704a0e6
CodCot <codrin.cotarlan@softwire.com>
2022-06-17 11:18:52 +0100

    feat: Create new GA4 property and use the correct one

--- SeaPublicWebsite/Constants.cs
+++ SeaPublicWebsite/Constants.cs
@@ -3,4 +3,6 @@
 public static class Constants
 {
     public const string SERVICE_NAME = "Improve your property's energy efficiency";
+    public const string GOOGLE_ANALYTICS_TAG_TEST = "G-RJC885GHSW";
+    public const string GOOGLE_ANALYTICS_TAG_PRODUCTION = "G-ZRTWVTRDRS";
 }
