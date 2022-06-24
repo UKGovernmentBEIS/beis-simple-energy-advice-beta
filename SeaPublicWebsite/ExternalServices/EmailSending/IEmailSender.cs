3d31a3623a23d77203ebc14c36db22ad7403dde8
Dan Corder <dan.corder@softwire.com>
2022-06-24 16:44:16 +0100

    feat: swap feedback pages to use Microsoft forms

--- SeaPublicWebsite/ExternalServices/EmailSending/IEmailSender.cs
+++ SeaPublicWebsite/ExternalServices/EmailSending/IEmailSender.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using SeaPublicWebsite.Models.Feedback;
-
+﻿
 namespace SeaPublicWebsite.ExternalServices.EmailSending
 {
     public interface IEmailSender
     {
         public void SendReferenceNumberEmail(string emailAddress, string reference);
         public void SendRequestedDocumentEmail(string emailAddress, byte[] documentContents);
-        public void SendFeedbackFormResponseEmail(string whatUserWasDoing, string whatUserToldUs);
-        public void SendFeedbackSurveyResponseEmail(FeedbackSurveyViewModel feedback);
-        
     }
 }
