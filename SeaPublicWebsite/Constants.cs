70ee43d7f9e739963048e0647d48cfb65988fff1
CodCot <codrin.cotarlan@softwire.com>
2022-07-05 09:58:01 +0100

    feat: Add accesibility page feedback url

--- SeaPublicWebsite/Constants.cs
+++ SeaPublicWebsite/Constants.cs
@@ -8,5 +8,6 @@ public static class Constants
     public const string FEEDBACK_URL_BANNER_RETURNING_USER = "https://forms.microsoft.com/Pages/ResponsePage.aspx?id=uFQMOXTjQkilS0DejzNYjZBO--vVRDJDluVZSKhFmVhUOEc5OVJXWlgxVjQxWVNIQlNUS1hDRlMyVC4u";
     public const string FEEDBACK_URL_REFERENCE_CODE_EMAIL = "https://forms.microsoft.com/Pages/ResponsePage.aspx?id=uFQMOXTjQkilS0DejzNYjZBO--vVRDJDluVZSKhFmVhUQkxHNkczR0wySUtQTzFHVEdZTDNYRzEzSS4u";
     public const string FEEDBACK_URL_ACTION_PLAN = "https://forms.microsoft.com/Pages/ResponsePage.aspx?id=uFQMOXTjQkilS0DejzNYjZBO--vVRDJDluVZSKhFmVhUMzlHSDg4M0pLTTlNS1lQSUYwVEcwMVEzSi4u";
+    public const string FEEDBACK_URL_ACCESSIBILITY_PAGE = "https://forms.microsoft.com/Pages/ResponsePage.aspx?id=uFQMOXTjQkilS0DejzNYjZBO--vVRDJDluVZSKhFmVhUMzdVTUkyWURaUEVRWUFHTjk1Vzg1M0pPWS4u";
     public const string SERVICE_NAME = "Improve your property's energy efficiency";
 }
