161b645dab81bd4a6fffc8c9648ae0545bd56c0f
David May-Miller <david.may-miller@softwire.com>
2023-09-05 17:23:49 +0100

    BEISSEA-NONE Don't download Chrome in .NET, that's done in Docker now. Also manually set address to localhost
    The previous method of getting the current adress was returning "http://[::]:80" which Chrome refuses to load. Will need to test in all environments to make sure the localhost address is fine but I expect it will be.

--- SeaPublicWebsite/Services/EnergyEfficiency/PdfGeneration/PdfGenerationService.cs
+++ SeaPublicWebsite/Services/EnergyEfficiency/PdfGeneration/PdfGenerationService.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.IO;
-using System.Linq;
+﻿using System.IO;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting.Server;
-using Microsoft.AspNetCore.Hosting.Server.Features;
 using Microsoft.Extensions.Options;
 using PuppeteerSharp;
 using PuppeteerSharp.Media;
@@ -14,14 +10,10 @@ namespace SeaPublicWebsite.Services.EnergyEfficiency.PdfGeneration;
 public class PdfGenerationService
 {
     private readonly BasicAuthMiddlewareConfiguration basicAuthConfiguration;
-    private readonly IServer server;
 
-    public PdfGenerationService(
-        IOptions<BasicAuthMiddlewareConfiguration> basicAuthOptions,
-        IServer server)
+    public PdfGenerationService(IOptions<BasicAuthMiddlewareConfiguration> basicAuthOptions)
     {
         basicAuthConfiguration = basicAuthOptions.Value;
-        this.server = server;
     }
 
     // This function runs a headless chrome browser without a sandbox (--no-sandbox).
@@ -30,12 +22,9 @@ public class PdfGenerationService
     // https://softwiretech.atlassian.net/browse/BEISSEA-73
     public async Task<Stream> GeneratePdf(string path)
     {
-        await new BrowserFetcher().DownloadAsync(BrowserFetcher.DefaultChromiumRevision);
         var launchOptions = new LaunchOptions
         {
             Headless = true,
-            IgnoreDefaultArgs = true,
-            IgnoredDefaultArgs = new[] { "--disable-extensions" },
             Args = new [] { "--no-sandbox" }
         };
         
@@ -58,6 +47,6 @@ public class PdfGenerationService
 
     private string GetLocalAddress()
     {
-        return server.Features.Get<IServerAddressesFeature>().Addresses.First();
+        return "http://localhost:80";
     }
 }
