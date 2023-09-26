ba2cc121b514bcd5b5b767ebeee6dd16c98c6b22
Taiwo <taiwo.olateju@softwire.com>
2023-09-26 15:51:23 +0100

    BEISSEA-85: Pull out middleware invoke into smaller functions

--- SeaPublicWebsite/Middleware/SecurityHeadersMiddleware.cs
+++ SeaPublicWebsite/Middleware/SecurityHeadersMiddleware.cs
@@ -16,31 +16,43 @@ public class SecurityHeadersMiddleware
 
     public Task Invoke(HttpContext context)
     {
+        CheckXContentTypeOptions(context);
+        CheckContentSecurityPolicy(context);
+        CheckReferrerPolicy(context);
+
+        return next(context);
+    }
+
+    private static void CheckXContentTypeOptions(HttpContext context)
+    {
         if (!context.Response.Headers.ContainsKey("X-Content-Type-Options"))
         {
             context.Response.Headers.Add("X-Content-Type-Options", "nosniff");
         }
-        
-        if (!context.Response.Headers.ContainsKey("Content-Security-Policy"))
-        {
-            var nonce = GenerateNonce();
-            context.Items.Add("ScriptNonce", nonce);
-            
-            context.Response.Headers.Add("Content-Security-Policy",
-                "default-src 'self'; " +
-                $"script-src 'self' https://*.googletagmanager.com 'nonce-{nonce}'; " +
-                "connect-src 'self' https://*.googletagmanager.com; " +
-                "img-src 'self' https://*.googletagmanager.com"); 
+    }
 
-        }
+    private static void CheckContentSecurityPolicy(HttpContext context)
+    {
+        if (context.Response.Headers.ContainsKey("Content-Security-Policy")) { return; }
         
+        var nonce = GenerateNonce();
+        context.Items.Add("ScriptNonce", nonce);
+            
+        context.Response.Headers.Add(
+            "Content-Security-Policy",
+            "default-src 'self'; " +
+            $"script-src 'self' https://*.googletagmanager.com 'nonce-{nonce}'; " +
+            "connect-src 'self' https://*.googletagmanager.com; " +
+            "img-src 'self' https://*.googletagmanager.com");
+    }
+
+    private static void CheckReferrerPolicy(HttpContext context)
+    {
         if (!context.Response.Headers.ContainsKey("Referrer-Policy"))
         {
             context.Response.Headers.Add("Referrer-Policy", "no-referrer");
 
         }
-
-        return next(context);
     }
 
     private static string GenerateNonce()
