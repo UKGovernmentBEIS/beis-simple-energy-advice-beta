b01de6493f045d36299fefb68c624fcebbfdc7bb
Glenn Clarke <glenn.clarke@softwire.com>
2024-08-07 11:59:17 +0100

    PC-1234: Fix capitalisation for consistency

--- SeaPublicWebsite.Data/Migrations/20240806095041_ForwardFillTimestamps.cs
+++ SeaPublicWebsite.Data/Migrations/20240806095041_ForwardFillTimestamps.cs
@@ -12,19 +12,19 @@ namespace SeaPublicWebsite.Data.Migrations
             migrationBuilder.Operations.Add(new SqlOperation
             {
                 Sql = @"
-                    with grouped_table as (
-                        SELECT ""PropertyDataId"", ""RecommendationsFirstRetrievedAt"", count(""RecommendationsFirstRetrievedAt"") 
+                    WITH grouped_table AS (
+                        SELECT ""PropertyDataId"", ""RecommendationsFirstRetrievedAt"", COUNT(""RecommendationsFirstRetrievedAt"") 
                         OVER (ORDER BY ""PropertyDataId"") AS _grp
                         FROM ""PropertyData""
                     ), 
-                    final_table AS(
+                    final_table AS (
                         SELECT ""PropertyDataId"", _grp, FIRST_VALUE(""RecommendationsFirstRetrievedAt"") 
-                            OVER (partition by _grp order by ""PropertyDataId"") AS smeared_timestamps FROM grouped_table
+                            OVER (PARTITION BY _grp ORDER BY ""PropertyDataId"") AS smeared_timestamps FROM grouped_table
                     )
                     UPDATE ""PropertyData"" p 
-                        SET ""RecommendationsFirstRetrievedAt"" = f.smeared_timestamps from final_table f 
+                        SET ""RecommendationsFirstRetrievedAt"" = f.smeared_timestamps FROM final_table f 
                         WHERE p.""PropertyDataId"" = f.""PropertyDataId"" 
-                            AND f.""smeared_timestamps"" >= to_timestamp('2024/08/01 00:00:00', 'YYYY/MM/DD HH24:MI:SS') AT TIME ZONE 'UTC';"
+                            AND f.""smeared_timestamps"" >= TO_TIMESTAMP('2024/08/01 00:00:00', 'YYYY/MM/DD HH24:MI:SS') AT TIME ZONE 'UTC';"
             });
         }
 
