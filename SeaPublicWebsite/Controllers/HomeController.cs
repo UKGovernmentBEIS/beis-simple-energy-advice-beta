3414c4188839bb4b6d535c674951ef23df4e601b
Joe Gage <joseph.gage@softwire.com>
2024-12-31 14:10:23 +0000

    Remove unused logger

--- SeaPublicWebsite/Controllers/HomeController.cs
+++ SeaPublicWebsite/Controllers/HomeController.cs
@@ -1,26 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 
-namespace SeaPublicWebsite.Controllers
+namespace SeaPublicWebsite.Controllers;
+
+public class HomeController : Controller
 {
-    public class HomeController : Controller
+    [HttpGet("/")]
+    public IActionResult Index()
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
-        [HttpGet("/")]
-        public IActionResult Index()
-        {
 #if DEBUG
-            return Redirect("energy-efficiency/new-or-returning-user");
+        return Redirect("energy-efficiency/new-or-returning-user");
 #endif
 #pragma warning disable CS0162 // Unreachable code detected
-            return Redirect("https://www.gov.uk/improve-energy-efficiency");
+        return Redirect("https://www.gov.uk/improve-energy-efficiency");
 #pragma warning restore CS0162
-        }
     }
 }
