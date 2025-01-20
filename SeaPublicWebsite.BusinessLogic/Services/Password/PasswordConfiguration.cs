9a3e6df2a6fbf8c6638daac06198cb1ab44eb376
Samuel Young <samuel.young@softwire.com>
2025-01-20 14:12:43 +0000

    PC-1431: Copy across the auth structure from PC-1432
    see https://github.com/UKGovernmentBEIS/desnz-warm-homes-local-grant/pull/36 for all new files

---
+++ SeaPublicWebsite.BusinessLogic/Services/Password/PasswordConfiguration.cs
@@ -0,0 +1,8 @@
+﻿namespace SeaPublicWebsite.BusinessLogic.Services.Password;
+
+public class PasswordConfiguration
+{
+    public const string ConfigSection = "Auth";
+
+    public string Password { get; set; }
+}
