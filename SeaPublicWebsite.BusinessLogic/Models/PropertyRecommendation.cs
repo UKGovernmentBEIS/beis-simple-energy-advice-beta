b778a220c6f75fdba5dd92d927d1755e76c036ec
Samuel Young <samuel.young@softwire.com>
2025-07-29 11:58:24 +0100

    PC-1707: Allow for existing recommendation sets to be updated
    track how these are updated for use in the frontend

--- SeaPublicWebsite.BusinessLogic/Models/PropertyRecommendation.cs
+++ SeaPublicWebsite.BusinessLogic/Models/PropertyRecommendation.cs
@@ -14,7 +14,19 @@ public class PropertyRecommendation : IEntityWithRowVersioning
     public string Summary { get; set; }
     public RecommendationAction? RecommendationAction { get; set; }
     public uint Version { get; set; }
-    
+
     public int PropertyDataId { get; set; }
     public PropertyData PropertyData { get; set; }
+
+    public bool HasSameImpactAs(PropertyRecommendation other)
+    {
+        if (other == null) return false;
+
+        return Key == other.Key &&
+               MinInstallCost == other.MinInstallCost &&
+               MaxInstallCost == other.MaxInstallCost &&
+               Saving == other.Saving &&
+               LifetimeSaving == other.LifetimeSaving &&
+               Lifetime == other.Lifetime;
+    }
 }
