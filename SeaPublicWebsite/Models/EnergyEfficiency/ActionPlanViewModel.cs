611767d548c38c6c747710aebfde2d40749e4eb8
Samuel Young <samuel.young@softwire.com>
2025-07-29 17:14:18 +0100

    PC-1707: Format touched files

--- SeaPublicWebsite/Models/EnergyEfficiency/ActionPlanViewModel.cs
+++ SeaPublicWebsite/Models/EnergyEfficiency/ActionPlanViewModel.cs
@@ -16,35 +16,46 @@ namespace SeaPublicWebsite.Models.EnergyEfficiency
         public bool IsPdf { get; set; }
         public string UrlPrefix { get; set; }
         public DateTime? LastUpdated { get; set; }
-        
+
         public string GetTotalInstallationCostText()
         {
-            var minCost = GetSavedRecommendations().Where(r => r.Key != RecommendationKey.InstallHeatPump).Sum(r => r.MinInstallCost);
-            var maxCost = GetSavedRecommendations().Where(r => r.Key != RecommendationKey.InstallHeatPump).Sum(r => r.MaxInstallCost);
+            var minCost = GetSavedRecommendations().Where(r => r.Key != RecommendationKey.InstallHeatPump)
+                .Sum(r => r.MinInstallCost);
+            var maxCost = GetSavedRecommendations().Where(r => r.Key != RecommendationKey.InstallHeatPump)
+                .Sum(r => r.MaxInstallCost);
             return $"£{minCost:N0} - £{maxCost:N0}";
         }
 
         public List<PropertyRecommendation> GetSavedRecommendations()
         {
-            return PropertyData.PropertyRecommendations.Where(r => r.RecommendationAction == RecommendationAction.SaveToActionPlan).ToList();
+            return PropertyData.PropertyRecommendations
+                .Where(r => r.RecommendationAction == RecommendationAction.SaveToActionPlan).ToList();
         }
+
         public List<PropertyRecommendation> GetDecideLaterRecommendations()
         {
-            return PropertyData.PropertyRecommendations.Where(r => r.RecommendationAction == RecommendationAction.DecideLater).ToList();
+            return PropertyData.PropertyRecommendations
+                .Where(r => r.RecommendationAction == RecommendationAction.DecideLater).ToList();
         }
+
         public List<PropertyRecommendation> GetDiscardedRecommendations()
         {
-            return PropertyData.PropertyRecommendations.Where(r => r.RecommendationAction == RecommendationAction.Discard).ToList();
+            return PropertyData.PropertyRecommendations
+                .Where(r => r.RecommendationAction == RecommendationAction.Discard).ToList();
         }
 
         public string GetTotalSavingText(IHtmlLocalizer<SharedResources> sharedLocalizer)
         {
-            var saving = GetSavedRecommendations().Where(r => r.Key != RecommendationKey.InstallHeatPump).Sum(r => r.Saving);
-            return string.Format(sharedLocalizer["£AYearString"].Value, $"{saving:N0}" );
+            var saving = GetSavedRecommendations().Where(r => r.Key != RecommendationKey.InstallHeatPump)
+                .Sum(r => r.Saving);
+            return string.Format(sharedLocalizer["£AYearString"].Value, $"{saving:N0}");
         }
+
         public string GetInstallationCostText(PropertyRecommendation recommendation)
         {
-            return recommendation.Key == RecommendationKey.InstallHeatPump ? "-" : $"£{recommendation.MinInstallCost:N0} - £{recommendation.MaxInstallCost:N0}";
+            return recommendation.Key == RecommendationKey.InstallHeatPump
+                ? "-"
+                : $"£{recommendation.MinInstallCost:N0} - £{recommendation.MaxInstallCost:N0}";
         }
 
         public string GetSavingText(PropertyRecommendation recommendation)
